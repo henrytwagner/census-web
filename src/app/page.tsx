cccaa6598c5c7afe6cd4fb0e16f5f34a83d1b6c6
henrytwagner <htwags22@gmail.com>
2025-08-24 22:17:55 -0400

    Adds header component html & styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Body } from "@/components/home/Body";
+import { Header } from "@/components/home/Header";
+
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-semibold">Census (Next.js) — Hello 👋</h1>
-      <p className="mt-2 text-gray-600">App Router + TypeScript + Tailwind</p>
+      <Header />
+      <Body />
     </main>
   );
 }
---
+++ src/components/home/Header.tsx
@@ -0,0 +1,26 @@
+export const Header = () => {
+    return (
+        <header className="flex p-2 justify-between items-center bg-gray-200">
+            <div className="flex items-center flex-shrink-0 gap-2 w-[300px]">
+                <div className="flex w-8 h-8 items-center flex-shrink-0 rounded-lg bg-white"></div>
+                <div className="w-[1px] h-8 bg-black"></div>
+                <div className="flex w-24 justify-between items-center flex-shrink-0">
+                    <div className="w-8 h-8 flex-shrink-0 rounded-lg bg-amber-500 z-50"></div>
+                    <div className="w-8 h-8 flex-shrink-0 rounded-lg bg-pink-500 -ml-3 z-40"></div>
+                    <div className="w-8 h-8 flex-shrink-0 rounded-lg bg-green-500 -ml-3 z-30"></div>
+                    <div className="w-8 h-8 flex-shrink-0 rounded-lg bg-blue-500 -ml-3 z-20"></div>
+                    <div className="w-8 h-8 flex-shrink-0 rounded-lg bg-red-500 -ml-3 z-10"></div>
+                </div>
+            </div>
+            <div className="flex h-6 p-1 justify-center items-center flex-shrink-0 w-90 bg-gray-700 rounded-full gap-1.5">
+                <p className="text-xs not-italic font-normal">􀊫</p>
+                <p className="text-xs not-italic font-normal">Search</p>
+            </div>
+            <div className="flex w-[300px] justify-end items-center gap-2.5 flex-shrink-0">
+                <p className="text-xl not-italic font-normal text-black">􀊫</p>
+                <div className="flex w-8 h-8 flex-col justify-center items-center flex-shrink-0 rounded-full bg-blue-700"></div>
+            </div>
+        </header>
+    );
+}
+

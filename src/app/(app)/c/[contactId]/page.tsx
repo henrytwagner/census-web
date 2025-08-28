44794d648a515a8bc03f88484ab7b3cfd542a839
henrytwagner <htwags22@gmail.com>
2025-08-28 09:35:01 -0400

    Fix sidebar scroll behavior

--- src/app/(app)/c/[contactId]/page.tsx
+++ src/app/(app)/c/[contactId]/page.tsx
@@ -2,7 +2,7 @@ import { Contact } from "@/app/(app)/_components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex items-start w-full h-full bg-bg-dark">
+    <main className="flex items-start w-full h-full bg-bg-dark overflow-hidden">
       <Contact />
     </main>
   );
